eff1c9b664d11f1d7bebe745863b7fe0ac29f2bf
Phan Trung Kien <kevinkien2706@gmail.com>
2025-08-18 11:58:15 -0400

    trial commit

---
+++ DBS501-lab1Result.sql
@@ -0,0 +1,13 @@
+-- Enable output
+SET SERVEROUTPUT ON;
+
+-- Test even_odd
+EXEC even_odd(4);
+EXEC even_odd(7);
+
+-- Test factorial
+EXEC factorial(5);
+EXEC factorial(0);
+EXEC factorial(-3);
+
+
